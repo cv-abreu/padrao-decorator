638fbcf62969b84c6f4ce55c7fc92c994b4c94ef
[Caroline Vitória Vieira de Abreu] <carolinevv01@gmail.com>
2025-09-08 12:25:01 -0300

    Update ProjectApplication.java

--- project/src/main/java/com/example/project/ProjectApplication.java
+++ project/src/main/java/com/example/project/ProjectApplication.java
@@ -14,10 +14,28 @@ public class ProjectApplication { //classe principal do projeto
 		SpringApplication.run(ProjectApplication.class, args);
 
 		System.out.println("> Exemplo");
+
+		//Criando pedido de pizza base
+		System.out.println("Pedido 1: Pizza Base");
 		Pizza myPizza = new PizzaBase();
 		System.out.println("Pedido: " + myPizza.getDescricao()); 
 		System.out.println("Preço: R$" + myPizza.getPreco());
 		System.out.println("-------------------------------------------");
+
+		//Adicionando Azeitonas
+		System.out.println("Pedido 2: Pizza Base + Azeitonas");
+		myPizza = new DecoradorAzeitona(myPizza); //decorando a pizza base com azeitonas
+		System.out.println("Pedido: " + myPizza.getDescricao());
+		System.out.println("Preço: R$" + myPizza.getPreco());
+		System.out.println("-------------------------------------------");
+
+		//Adicionando Tomates
+		System.out.println("Pedido 3: Pizza Base + Azeitonas + Tomates");
+		myPizza = new DecoradorTomate(myPizza); //decorando a pizza base com tomates
+		System.out.println("Pedido: " + myPizza.getDescricao());
+		System.out.println("Preço: R$" + myPizza.getPreco());
+		System.out.println("-------------------------------------------");
+
 	}
 
 		
